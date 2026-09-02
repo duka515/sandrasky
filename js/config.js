fb92fcc8c20a805afcaee51a7c1691598caa632e
duka515 <duka.jet@gmail.com>
2026-09-03 00:56:43 +0200

    Fix affiliate URLs and popup gif paths

--- js/config.js
+++ js/config.js
@@ -5,12 +5,12 @@ window.SITE = {
   videos: 241,
   photos: 512,
   avatar: "assets/avatar.jpeg",
-  liveUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&amp;aff_sub5=SF_006OG00000NbNlB&amp;aff_sub=TTT",
-  liveImage: "assets/popup-live.svg",
-  premiumUrl: "https://example.com/premium",
-  premiumImage: "assets/popup-premium.svg",
-  photosUrl: "https://example.com/photos",
-  photosImage: "assets/popup-photos.svg",
+  liveUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT",
+  liveImage: "assets/popup-live.gif",
+  premiumUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT",
+  premiumImage: "assets/popup-premium.gif",
+  photosUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT",
+  photosImage: "assets/popup-photos.gif",
   popup: {
     enabled: true,
     brand: "Jerkmate",
@@ -20,8 +20,8 @@ window.SITE = {
       "2- Get the Free Lifetime Membership",
       "3- Enjoy thousands of models like me"
     ],
-    image: "assets/popup.svg",
+    image: "assets/popup-photos.gif",
     button: "Join for Free",
-    url: "https://example.com/join"
+    url: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT"
   }
 };
